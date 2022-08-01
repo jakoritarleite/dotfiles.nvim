3897f44e004229ff642ae4e8740399d2bbff27d3
João Koritar <jakoritarleite@gmail.com>
2022-08-01 18:27:49 -0300

    feat: add some remaps such as go to definition and hover

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,3 +3,11 @@ local nnoremap = require("keymap").nnoremap
 -- open nvim explorer with Space P V
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+-- go to definition
+nnoremap("<leader>dd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+
+-- show hover
+nnoremap("<leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>")
+
+-- code actions
+nnoremap("<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>")
