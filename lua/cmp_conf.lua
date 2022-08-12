7bf1044d83f5942cc36a7c7dd329be1dccdce86e
Joao Koritar <jakoritarleite@gmail.com>
2022-08-12 16:17:56 -0300

    feat: change the cmp autocomplete key

--- lua/cmp_conf.lua
+++ lua/cmp_conf.lua
@@ -13,7 +13,7 @@ cmp.setup({
         ['<C-Down>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-Enter>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources(
         {
