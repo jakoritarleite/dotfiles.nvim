5e45d9c0b7ad209fe2963e64d6230e48b5ae00a8
Joao Koritar <jakoritarleite@gmail.com>
2022-12-20 12:01:05 -0300

    feat: customize lualine to my taste

--- after/plugin/lline.lua
+++ after/plugin/lline.lua
@@ -1,5 +1,25 @@
 require("lualine").setup {
     options = {
-        icons_enabled = true
-    }
+        icons_enabled = false,
+        section_separators = '',
+        component_separators = '',
+        theme = 'gruvbox_dark',
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {'diff', 'diagnostics'},
+        lualine_c = {'encoding', 'fileformat'},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    },
 }
+
