b03c95ff57329ea5fba08a18fe0f2cac1212aeba
Joao Koritar <jakoritarleite@gmail.com>
2023-01-14 15:08:34 -0300

    Remove old after/plugin folder

--- after/plugin/mason.lua
+++
@@ -1,4 +0,0 @@
-require("mason").setup({})
-require("mason-lspconfig").setup({
-    ensure_installed = { "sumneko_lua", "rust_analyzer", "clangd", "cmake", "dockerls", "eslint", "gopls", "jsonls", "tsserver", "pyright", "lemminx", "yamlls" }
-})
